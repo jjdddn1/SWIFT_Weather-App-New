3173417ad8c9a263e35d2ad03b5ab0d07267fe7c
Huiyuan Ren <huiyuanr@usc.edu>
2016-04-19 22:50:40 -0700

    complete delete function

--- Weather App/SearchHistoryViewController.swift
+++ Weather App/SearchHistoryViewController.swift
@@ -15,12 +15,12 @@ class SearchHistoryViewController: UIViewController {
 
     @IBOutlet weak var NavBar: UIView!
     
-    @IBOutlet weak var Button1: SpringButton!
-    @IBOutlet weak var Button2: SpringButton!
-    @IBOutlet weak var Button3: SpringButton!
-    @IBOutlet weak var Button4: SpringButton!
-    @IBOutlet weak var Button5: SpringButton!
-    @IBOutlet weak var Button6: SpringButton!
+    @IBOutlet weak var Button1: UIButton!
+    @IBOutlet weak var Button2: UIButton!
+    @IBOutlet weak var Button3: UIButton!
+    @IBOutlet weak var Button4: UIButton!
+    @IBOutlet weak var Button5: UIButton!
+    @IBOutlet weak var Button6: UIButton!
 
     
     @IBOutlet weak var Delete1: UIButton!
@@ -30,19 +30,31 @@ class SearchHistoryViewController: UIViewController {
     @IBOutlet weak var Delete5: UIButton!
     @IBOutlet weak var Delete6: UIButton!
 
+    @IBOutlet weak var View1: SpringView!
+    @IBOutlet weak var View2: SpringView!
+    @IBOutlet weak var View3: SpringView!
+    @IBOutlet weak var View4: SpringView!
+    @IBOutlet weak var View5: SpringView!
+    @IBOutlet weak var View6: SpringView!
+
+    
+    
+    
     var buttonArray : [UIButton] = []
     var deleteArray : [UIButton] = []
+    var buttonPosition: [Int] = []
+    var viewArray: [UIView] = []
     
     @IBOutlet weak var backGroundButton: UIButton!
     
     @IBOutlet weak var editButton: UIButton!
     
+    var distance :CGFloat = 0
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        buttonArray = [Button1, Button2, Button3, Button4, Button5, Button6]
-        deleteArray = [Delete1, Delete2, Delete3, Delete4, Delete5, Delete6]
-        
-        setUpUI()
+
         // Do any additional setup after loading the view.
     }
 
@@ -51,30 +63,31 @@ class SearchHistoryViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     override func viewDidAppear(animated: Bool) {
-
+        distance = self.viewArray[0].center.y - self.viewArray[1].center.y
+        
     }
     
     override func viewWillAppear(animated: Bool) {
+        deleteArray = [Delete1, Delete2, Delete3, Delete4, Delete5, Delete6]
+        buttonArray = [Button1, Button2, Button3, Button4, Button5, Button6]
+        viewArray = [View1,View2,View3, View4, View5, View6]
+        buttonPosition = [0,1,2,3,4,5]
+        setUpUI()
+        
         UIView.animateWithDuration(0.4) {
             self.NavBar.transform = CGAffineTransformMakeTranslation(0,0)
         }
-//        for del in deleteArray {
-//            del.hidden = true
-//            del.alpha = 0
-//        }
+
     }
     
     override func viewWillDisappear(animated: Bool) {
         let height = NavBar.frame.height
         UIView.animateWithDuration(0.3) {
             self.NavBar.transform = CGAffineTransformMakeTranslation(0,-height)
-            for but in self.buttonArray {
-                but.transform = CGAffineTransformMakeTranslation(0,100)
+            for view in self.viewArray {
+                view.transform = CGAffineTransformMakeTranslation(0,100)
             }
-            for del in self.deleteArray {
-                del.transform = CGAffineTransformMakeTranslation(0,-100)
 
-            }
 
         }
         
@@ -98,55 +111,56 @@ class SearchHistoryViewController: UIViewController {
     
     @IBAction func deleteButtonPressed(sender: UIButton) {
         let n = Int(sender.restorationIdentifier!)
-        let distance = self.buttonArray[0].center.y - self.buttonArray[1].center.y
         for del in deleteArray {
             del.enabled = false
         }
         
         UIView.animateWithDuration(0.2, animations: {
-            self.buttonArray[n!].transform = CGAffineTransformMakeTranslation(-100,0)
-            self.deleteArray[n!].transform = CGAffineTransformMakeTranslation(-100,0)
-            self.buttonArray[n!].alpha = 0
-            self.deleteArray[n!].alpha = 0
-            }) { (Bool) in
-                
-                self.buttonArray[n!].transform = CGAffineTransformMakeTranslation(0,0)
-                self.buttonArray[n!].center = CGPoint(x: (self.buttonArray.last?.center.x)!, y: (self.buttonArray.last?.center.y)! - distance)
-                self.buttonArray[n!].alpha = 0
-                
-                self.deleteArray[n!].transform = CGAffineTransformMakeTranslation(0,0)
-                self.deleteArray[n!].center = CGPoint(x: (self.deleteArray.last?.center.x)!, y: (self.deleteArray.last?.center.y)! - distance)
-                self.deleteArray[n!].alpha = 0
-                
-                
-                UIView.animateWithDuration(0.2){
-                    for i in (n! + 1) ..<  self.buttonArray.count {
-                        self.buttonArray[i].center = CGPoint(x: self.buttonArray[i].center.x,y: self.buttonArray[i].center.y + distance)
-                        self.deleteArray[i].center = CGPoint(x: self.deleteArray[i].center.x,y: self.deleteArray[i].center.y + distance)
-                    }
-                    self.buttonArray[n!].center = CGPoint(x: self.buttonArray[n!].center.x,y: self.buttonArray[n!].center.y + distance)
-                    self.buttonArray[n!].alpha = 1
-                    
-                    self.deleteArray[n!].center = CGPoint(x: self.deleteArray[n!].center.x,y: self.deleteArray[n!].center.y + distance)
-                    self.deleteArray[n!].alpha = 1
+            let offset2 = n! - self.buttonPosition[n!]
 
-                }
-                let button = self.buttonArray.removeAtIndex(n!)
-                self.buttonArray.append(button)
+            self.viewArray[n!].transform = CGAffineTransformMakeTranslation(-100, -CGFloat(offset2) * self.distance)
+            self.viewArray[n!].alpha = 0
+            
+            }) { (Bool) in
+                let offset = 6 - self.buttonPosition[n!]
+                self.viewArray[n!].transform = CGAffineTransformMakeTranslation(0, -self.distance * CGFloat(offset))
+
                 
-                let delete = self.deleteArray.removeAtIndex(n!)
-                self.deleteArray.append(delete)
-                
-                for i in 0 ..< self.deleteArray.count{
-                    self.deleteArray[i].restorationIdentifier = "\(i)"
-                    self.deleteArray[i].enabled = true
+                UIView.animateWithDuration(0.2, animations: {
+
+//                    print(self.distance)
+                    for i in (n!) ..<  self.viewArray.count {
+                        self.viewArray[i].transform = CGAffineTransformTranslate(self.viewArray[i].transform, 0, self.distance)
+                    }
+                    self.viewArray[n!].alpha = 1
+
+                }) { (Bool) in
+                    let button = self.buttonArray.removeAtIndex(n!)
+                    self.buttonArray.append(button)
+                    
+                    let delete = self.deleteArray.removeAtIndex(n!)
+                    self.deleteArray.append(delete)
+                    
+                    let pos = self.buttonPosition.removeAtIndex(n!)
+                    self.buttonPosition.append(pos)
+                    
+                    let view = self.viewArray.removeAtIndex(n!)
+                    self.viewArray.append(view)
+                    
+                    for i in 0 ..< self.deleteArray.count{
+                        self.deleteArray[i].restorationIdentifier = "\(i)"
+                        self.deleteArray[i].enabled = true
+                    }
                 }
+
         }
     }
     
     var userIsEditing = false
     @IBAction func editButtonPressed(sender: UIButton) {
         if(!userIsEditing){
+            self.editButton.setTitle("Done", forState: UIControlState.Normal)
+
             backGroundButton.enabled = false
             userIsEditing = true
             for button in buttonArray{
@@ -165,6 +179,7 @@ class SearchHistoryViewController: UIViewController {
         }
         else{
             userIsEditing = false
+            self.editButton.setTitle("Edit", forState: UIControlState.Normal)
 
             UIView.animateWithDuration(0.2, animations: {
                 for del in self.deleteArray {
@@ -184,11 +199,10 @@ class SearchHistoryViewController: UIViewController {
 
         }
         
-        if(userIsEditing){
-            self.editButton.setTitle("Edit", forState: UIControlState.Normal)
-        }else{
-            self.editButton.setTitle("Done", forState: UIControlState.Normal)
-        }
+//        if(userIsEditing){
+//            self.editButton.setTitle("Edit", forState: UIControlState.Normal)
+//        }else{
+//        }
     }
 
     
